2e1058c6797c08afa9a38c84e2f368dd0569a504
Tarcisio <caninosbrancos10031@gmail.com>
2022-09-18 23:07:31 -0300

    organizando

--- progeto1/src/index.js
+++ progeto1/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/global-styles.css';
+import {Home} from './templates/HOME/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
-reportWebVitals();
---
+++ progeto1/src/templates/HOME/index.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import './styles.css';
+// import { PostCard } from './components/PostCard';
+import { Posts } from '../../components/Posts';
+import {loadPosts} from '../../ultil/loas-post';
+
+export class Home extends Component {
+  state = {
+    posts: []
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+const postsAndPhotos = await loadPosts();
+   
+this.setState({ posts: postsAndPhotos});
+
+}
+
+render() {
+    const { posts } = this.state;
+    return (
+
+      <section className='container'>
+      <Posts posts={posts}/>
+      </section>
+
+    );
+  }
+}
+

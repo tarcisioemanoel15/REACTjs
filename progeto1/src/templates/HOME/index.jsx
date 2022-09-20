161310e9388847cddb833809dbf13ef2f5139610
Tarcisio <caninosbrancos10031@gmail.com>
2022-09-19 21:10:42 -0300

    Paginação

--- progeto1/src/templates/HOME/index.jsx
+++ progeto1/src/templates/HOME/index.jsx
@@ -2,11 +2,16 @@ import { Component } from 'react';
 import './styles.css';
 // import { PostCard } from './components/PostCard';
 import { Posts } from '../../components/Posts';
-import {loadPosts} from '../../ultil/loas-post';
+import { loadPosts } from '../../ultil/loas-post';
+import { Button } from '../../Button';
 
 export class Home extends Component {
   state = {
-    posts: []
+    posts: [],
+    allPosts: [],
+
+    page: 0,
+    postsPerPage: 3
   };
 
   async componentDidMount() {
@@ -14,18 +19,55 @@ export class Home extends Component {
   }
 
   loadPosts = async () => {
-const postsAndPhotos = await loadPosts();
-   
-this.setState({ posts: postsAndPhotos});
+    const { page, postsPerPage } = this.state;
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos
 
-}
 
-render() {
-    const { posts } = this.state;
+    });
+
+  }
+
+
+  loadMorePosts = () => {
+    const {
+
+      page,
+      postsPerPage,
+      allPosts,
+      posts
+    } = this.state
+
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  }
+
+  render() {
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
     return (
 
       <section className='container'>
-      <Posts posts={posts}/>
+        <Posts posts={posts} />
+
+        <div className='button-container'>
+
+
+          <Button
+            text="Load M Posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+
+          />
+
+        </div>
+
+
       </section>
 
     );

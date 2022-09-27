c405e7b678631c2bbfe5d938f87072d48b06fa55
Tarcisio <caninosbrancos10031@gmail.com>
2022-09-27 20:39:32 -0300

    tete post card

---
+++ refazendoProgeto1/src/components/PostCard/Post.spec.jsx
@@ -0,0 +1,10 @@
+import {render} from '@testing-library/react';
+import {PostCard} from '.'
+
+describe('<PostCard />', () => {
+    it('should render POstCard correcty', () => {
+
+        render(<PostCard />);
+    })
+
+}) 

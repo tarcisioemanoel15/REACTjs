88ba3f06f4404c7978d924df3bd013fc38f9d4a3
Tarcisio <caninosbrancos10031@gmail.com>
2022-09-30 00:55:55 -0300

    instalando novas dependencias

---
+++ projeto3dependencias/src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+export const PostCard = ({title, cover, body, id}) => (
+    <div className='post'>
+
+        <img src={cover} alt={title}></img>
+        <div className="post-content">
+            <h2>{title}</h2>
+            <p>{body}</p>
+        </div>
+
+    </div>
+);

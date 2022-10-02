3508076fd3c6d34573fad556207d1786cf4e9364
Tarcisio <caninosbrancos10031@gmail.com>
2022-10-02 15:40:16 -0300

    commit

--- projeto3dependencias/src/index.js
+++ projeto3dependencias/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global-styles.css';
-import {Home} from './templates/HOME/index';
+import { Home } from './templates/Home/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

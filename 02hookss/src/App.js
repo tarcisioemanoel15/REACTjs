d880550474f6e7d8c67163e5dfedf7a41296d379
Tarcisio <caninosbrancos10031@gmail.com>
2022-12-14 22:28:13 -0300

    Iniciando useMemo

--- 02hookss/src/App.js
+++ 02hookss/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 // import UseEffect from './Components/UseEffect';
-import UseCallback from './UseCallback';
+import UseCallback from './Components/UseCallback';
+import UseMemo from './Components/UseMemo';
 // import UseEstate from './Components/UseState/00state';
 
 function App() {
   return (
     <div className="App">
       <h1>Tarcisio Emanoel</h1>
+      <UseMemo />
       <UseCallback />
       {/* <UseEffect /> */}
       {/* <UseEstate /> */}
---
+++ 02hookss/src/Components/UseMemo/index.jsx
@@ -0,0 +1,3 @@
+export default function UseMemo() {
+  return <h1>Use Memo</h1>;
+}

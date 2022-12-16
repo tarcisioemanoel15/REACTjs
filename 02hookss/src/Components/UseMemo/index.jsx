12c579bf1f2e428137717929fb65fbe0b8125e21
Tarcisio <caninosbrancos10031@gmail.com>
2022-12-15 23:08:40 -0300

    UseMemo hoock finalizado

--- 02hookss/src/Components/UseMemo/index.jsx
+++ 02hookss/src/Components/UseMemo/index.jsx
@@ -1,3 +1,33 @@
+import { useMemo } from 'react';
+import { useState } from 'react';
+
 export default function UseMemo() {
-  return <h1>Use Memo</h1>;
+  console.log('PAI RENDERIZOU');
+
+  const [value, setValue] = useState('');
+
+  return (
+    <div className="containerPosts">
+      <p>digitando no input filho renderiza so uma vez ver no console</p>
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+
+      {useMemo(() => {
+        return (
+          <div className="cont">
+            <h1>Use Memo</h1>
+            <p>
+              OBS: é so envolver o componente que vc quer que não re renderize
+            </p>
+            {console.log('FILHOOOO RENDERIZOU')}
+          </div>
+        );
+      }, [])}
+    </div>
+  );
 }
